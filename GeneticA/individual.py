ab8ac00eb10a1a23f95ffbd402fefdea98df7e36
HamzaSardar <43338940+HamzaSardar@users.noreply.github.com>
2020-04-10 15:13:58 +0100

    Create Individual class
    Produces individual with random position between upper and lower bounds, can take list input

---
+++ GeneticA/individual.py
@@ -0,0 +1,15 @@
+import random
+
+
+class Individual:
+
+    def __init__(self, lower_bound, upper_bound):
+
+        if len(lower_bound) != len(upper_bound):
+            raise ValueError('yuor lowe bound and uppe bound not same lemgth')
+
+        self.position = []
+        for i in range(0, len(lower_bound)):
+            self.position.append(random.uniform(lower_bound[i], upper_bound[i]))
+
+        self.fitness = None
